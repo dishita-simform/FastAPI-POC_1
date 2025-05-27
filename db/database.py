8bcd5e3699599ef2ed7f4bfbc145a67597cd7e77
dishita-simform <dishita.tank@simformsolutions.com>
2025-05-27 14:56:40 +0530

    Implemented Docker

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/to-do-poc"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/to-do-poc"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL) 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
